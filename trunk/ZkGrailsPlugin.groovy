b2f9d574de1d4061ca2cb628bb79ace1aea29602
chanwit <chanwit@c6677543-3d50-0410-bfd5-271c77d241ff>
2008-08-19 19:02:53 +0000

    merge with master

--- trunk/ZkGrailsPlugin.groovy
+++ trunk/ZkGrailsPlugin.groovy
@@ -1,12 +1,13 @@
 class ZkGrailsPlugin {
-	
-    def version = 0.4
+
+    def version = 0.5
+
     def dependsOn = [:]
 
     // TODO Fill in these fields
     def author = "chanwit"
     def authorEmail = ""
-    def title = "ZK (3.0.6-beta) for Grails"
+    def title = "ZK (3.0.7) for Grails"
     def description = '''\
 Derived from Flyisland ZK Grails Plugin, This plugin adds ZK Ajax framework (www.zkoss.org) support into Grails applications
 '''
@@ -17,67 +18,67 @@ Derived from Flyisland ZK Grails Plugin, This plugin adds ZK Ajax framework (www
     def doWithSpring = {
         // TODO Implement runtime spring config (optional)
     }
-   
+
     def doWithApplicationContext = { applicationContext ->
-        
+
     }
 
     def doWithWebDescriptor = { xml ->
-    	// quick hack for page filtering
-    	def pageFilter = xml.filter.find { it.'filter-name' == 'sitemesh'}    	
-    	pageFilter.'filter-class'.replaceBody('ZKGrailsPageFilter')    	
-    	
-		def listenerElements = xml.'listener'[0]
-		listenerElements + {
-			'listener' {
-				'display-name' ("ZK Session Cleaner")
-				'listener-class' ("org.zkoss.zk.ui.http.HttpSessionListener")
-			}
-		}
-		
-		def servletElements = xml.'servlet'[0]
-		def mappingElements = xml.'servlet-mapping'[0]
-		
-		servletElements + {
-			'servlet' {
-				'servlet-name' ("zkLoader")
-				'servlet-class' ("org.zkoss.zk.ui.http.DHtmlLayoutServlet")
-				'init-param' {
-					'param-name' ("update-uri")
-					'param-value' ("/zkau")
-				}
-				'load-on-startup' (0)
-			}
-		}
-		
-		def urls = ["*.zul", "*.zhtml", "*.svg", "*.xml2html"]
-		urls.each {p ->
-			mappingElements + {
-				'servlet-mapping' {
-				'servlet-name'("zkLoader")
-				'url-pattern'("${p}")
-				}
-			}
-		}
-		
-		servletElements + {
-			'servlet' {
-				'servlet-name' ("auEngine")
-				'servlet-class' ("org.zkoss.zk.au.http.DHtmlUpdateServlet")
-			}
-		}
-		mappingElements + {
-			'servlet-mapping' {
-			'servlet-name'("auEngine")
-			'url-pattern'("/zkau/*")
-			}
-		}
+        // quick hack for page filtering
+        def pageFilter = xml.filter.find { it.'filter-name' == 'sitemesh'}
+        pageFilter.'filter-class'.replaceBody('org.zkoss.zkgrails.ZKGrailsPageFilter')
+
+        def listenerElements = xml.'listener'[0]
+        listenerElements + {
+            'listener' {
+                'display-name' ("ZK Session Cleaner")
+                'listener-class' ("org.zkoss.zk.ui.http.HttpSessionListener")
+            }
+        }
+
+        def servletElements = xml.'servlet'[0]
+        def mappingElements = xml.'servlet-mapping'[0]
+
+        servletElements + {
+            'servlet' {
+                'servlet-name' ("zkLoader")
+                'servlet-class' ("org.zkoss.zk.ui.http.DHtmlLayoutServlet")
+                'init-param' {
+                    'param-name' ("update-uri")
+                    'param-value' ("/zkau")
+                }
+                'load-on-startup' (0)
+            }
+        }
+
+        def urls = ["*.zul", "*.zhtml", "*.svg", "*.xml2html"]
+        urls.each {p ->
+            mappingElements + {
+                'servlet-mapping' {
+                'servlet-name'("zkLoader")
+                'url-pattern'("${p}")
+                }
+            }
+        }
+
+        servletElements + {
+            'servlet' {
+                'servlet-name' ("auEngine")
+                'servlet-class' ("org.zkoss.zk.au.http.DHtmlUpdateServlet")
+            }
+        }
+        mappingElements + {
+            'servlet-mapping' {
+            'servlet-name'("auEngine")
+            'url-pattern'("/zkau/*")
+            }
+        }
     }
-	                                      
+
     def doWithDynamicMethods = { ctx ->
         // TODO Implement registering dynamic methods to classes (optional)
     }
-	
+
     def onChange = { event ->
         // TODO Implement code that is executed when any artefact that this plugin is
         // watching is modified and reloaded. The event contains: event.source,
